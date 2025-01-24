f61f147c7ea8ee88f2cc1412b3a31eb03dd851a6
18Mandaloriano <zaralandog@gmail.com>
2025-01-24 19:10:52 +0100

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HELLOWORD : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //me gustaria crar un juegazo pero así se empiaza
+        print("Hello, World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
